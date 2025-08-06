e1a5fed0c85687a24bd2561549824c8d69a6104e
C-503 <yeffersonv503@gmail.com>
2025-08-06 17:28:00 -0600

    creacion de proyecto, conexion a la base de datos y plantilla a utilizar

---
+++ app/config.php
@@ -0,0 +1,22 @@
+<?php
+
+
+define('SERVIDOR','localhost');
+define('USUARIO','root');
+define('PASSWORD','');
+define('BD','sistema_ventas');
+
+
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+
+try {
+
+    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    echo "Conexión exitosa a la base de datos C-503";
+
+} catch (PDOException $e) {
+
+    //print_r($e);
+    echo "Error de conexión a la base de datos C-503 ";
+
+}
