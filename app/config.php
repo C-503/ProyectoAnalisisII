80115fa1b0e3daddfb9d6ae3935c3a1e155dd91c
C-503 <yeffersonv503@gmail.com>
2025-08-27 23:03:18 -0600

    categorias y productos

--- app/config.php
+++ app/config.php
@@ -1,24 +1,26 @@
 <?php
 
-// Conexión a la base de datos en Railway usando variables de entorno
-define('SERVIDOR', getenv("MYSQLHOST"));
-define('USUARIO', getenv("MYSQLUSER"));
-define('PASSWORD', getenv("MYSQLPASSWORD"));
-define('BD', getenv("MYSQLDATABASE"));
-define('PUERTO', getenv("MYSQLPORT"));
+// conexion a la base de datos C503
+define('SERVIDOR','localhost');
+define('USUARIO','root');
+define('PASSWORD','');
+define('BD','sistema_ventas');
 
-$servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR . ";port=" . PUERTO;
+
+$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
 
 try {
-    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-    ));
-    // echo "Conexión exitosa a la base de datos en Railway";
+
+    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+   // echo "Conexión exitosa a la base de datos C-503";
+
 } catch (PDOException $e) {
-    echo "❌ Error de conexión a la base de datos: " . $e->getMessage();
+
+    //print_r($e);
+    echo "Error de conexión a la base de datos C-503 ";
 }
 
-$URL = "https://tu-proyecto-production.up.railway.app"; // cambia por la URL de Railway
+$URL = "http://localhost/proyecto%20analisis%20C-503";
 
 date_default_timezone_set("America/Guatemala");
 $fechaHora = date("Y-m-d h:i:s");
--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 include ('app/config.php');
 include ('layout/sesion.php');
 
-include ('layout/parte1.php'); ?>
+include ('layout/parte1.php'); 
+include ('app/controllers/usuario/listado_de_usuarios.php');
+include ('app/controllers/roles/listado_de_roles.php');
+
+?>
 
  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -11,7 +15,7 @@ include ('layout/parte1.php'); ?>
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-12">
-            <h1 class="m-0">Bienvenido a JEY Software GT</h1>
+            <h1 class="m-0">Bienvenido a JEY Software GT - <?php echo $rol_sesion;?></h1>
           </div><!-- /.col -->
          
         </div><!-- /.row -->
@@ -23,6 +27,63 @@ include ('layout/parte1.php'); ?>
     <div class="content">
       <div class="container-fluid">
         Clemcito
+        <br>
+        <br>
+
+        <div class="row">
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+
+                <?php 
+                  $contador_usuarios = 0;
+                  foreach($datos_usuarios as $dato_usuario){
+                    $contador_usuarios = $contador_usuarios + 1;
+                  }
+                ?>
+                <h3><?php echo $contador_usuarios?></h3>
+
+                <p>Usuarios Registrados</p>
+              </div>
+              <a href="<?php echo $URL;?>/usuarios/create.php">
+                <div class="icon">
+                <i class="fas fa-user-plus"></i>
+              </div>
+              </a>
+              <a href="<?php echo $URL;?>/usuarios" class="small-box-footer">Mas informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+
+                <?php 
+                  $contador_roles = 0;
+                  foreach($datos_roles as $dato_rol){
+                    $contador_roles = $contador_roles + 1;
+                  }
+                ?>
+                <h3><?php echo $contador_roles?></h3>
+
+                <p>Roles Registrados</p>
+              </div>
+              <a href="<?php echo $URL;?>/roles/create.php">
+                <div class="icon">
+                <i class="fas fa-user-plus"></i>
+              </div>
+              </a>
+              <a href="<?php echo $URL;?>/roles" class="small-box-footer">Mas informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+         
+          <!-- ./col -->
+        </div>
+
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
