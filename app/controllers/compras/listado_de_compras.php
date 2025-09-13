2356d3cb6eadeb511d7480b6f157ef1f33e26864
C-503 <yeffersonv503@gmail.com>
2025-09-12 22:19:16 -0600

    correccion de bug

--- app/controllers/compras/listado_de_compras.php
+++ app/controllers/compras/listado_de_compras.php
@@ -8,7 +8,7 @@
                     pro.imagen as imagen_producto,
                     cat.nombre_categoria as nombre_categoria, us.nombres as nombre_usuario, prov.nombre_proveedor as nombre_proveedor_compra,
                     prov.celular as celular_proveedor, prov.telefono as telefono_proveedor, prov.empresa as empresa_proveedor, prov.email as email_proveedor,
-                    prov.direccion as direccion_proveedor, us.
+                    prov.direccion as direccion_proveedor
                     FROM tb_compras as co INNER JOIN tb_almacen as pro 
     ON co.id_producto = pro.id_producto INNER JOIN tb_categoria as cat 
     ON cat.id_categoria = pro.id_categoria INNER JOIN tb_usuarios as us ON co.id_usuario = us.id_usuario INNER JOIN tb_proveedores as prov 
